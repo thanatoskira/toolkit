74b35563b8ab169579220dc80e8a4e44f0206a2e
thanat0s <thanatoskirakira@gmail.com>
2015-05-17 11:24:11 +0800

    DirBuster-version-0.1.1

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # toolkit
 Collection of tools
+
+Contact me at:
+    Twitter: @thanato233
+    Email: thanatoskirakira@gmail.com
+    Sina WeiBo: thanatoskira
+
+Change log
+==========
+
+- Addition of the DirBuster - T00l for Web Directory Scan (Developing)
+    - Addition of the Banner (2015/5/17)
+
+Submitting Issues
+=================
+If you have *questions* regarding the T00l please eamil me at thanatoskirakira@gmail.com
+
+If you find a *bug* please open an issue and include at least the following in the description:
+    - Full command string you used
+    - OS you using 
--- dirspider.py
+++ dirspider.py
@@ -6,9 +6,10 @@ DirBuster_version = 0.1
 
 import argparse
 import sys
+import random
 
-
-banner = """
+class Banners:
+    banner1 = """
     ____  _      ____             __           
    / __ \(_)____/ __ )__  _______/ /____  _____
   / / / / / ___/ __  / / / / ___/ __/ _ \/ ___/
@@ -17,13 +18,29 @@ banner = """
                                                
 """                                              
 
+    banner2 = """
+< Welcome to DirBuster >
+ ----------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\\
+                ||----w |
+                ||     ||
+"""
+    def printBanner(self):
+        banners = [self.banner1,self.banner2]
+        print random.choice(banners)
+
+
+
 if __name__ == '__main__':
-    print banner
+    Banners().printBanner()
     if len(sys.argv) == 1:
         sys.exit("usage:\n\tpython %s -h" % sys.argv[0])
-    parser = argparse.ArgumentParser(description="DirBuster v{} - Framework for Web Directory Scan".format(DirBuster_version), version=DirBuster_version, usage='',fromfile_prefix_chars='@', epilog="Time to go~")
+    parser = argparse.ArgumentParser(description="DirBuster v{} - T00l for Web Directory Scan".format(DirBuster_version), version=DirBuster_version, usage='',fromfile_prefix_chars='@', epilog="Time to go~")
     mgroup = parser.add_argument_group("DirBuster", "Options for DirBuster")
     mgroup.add_argument("-u","--url =",metavar="URL",dest="URL",help="The Target URL(eg:http://www.baidu.com/)")
     mgroup.add_argument("-w","--write",dest="OutFile", type=argparse.FileType('w'),metavar="filename", default=sys.stdout, help="Specify file to log to (stdout by default).")
     mgroup.add_argument("-f","--file =",dest="InFile", type=argparse.FileType('r'),metavar="filename", default=sys.stdin, help="The path of directory file.")
     args = parser.parse_args() 
+                
