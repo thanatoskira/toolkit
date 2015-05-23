5ca1c6252e98428b3af85245c7885a3b80fede00
thanat0s <thanatoskirakira@gmail.com>
2015-05-23 17:27:12 +0800

    Dirbuster_version_0.1.4

--- dirspider.py
+++ dirspider.py
@@ -9,6 +9,7 @@ import sys
 import random
 import requests
 import urlparse
+from time import ctime
 
 class Banners:
     banner1 = """
@@ -33,13 +34,40 @@ class Banners:
         banners = [self.banner1,self.banner2]
         print random.choice(banners)
 
-def Scanner(url):
-    parser = urlparse.urlparse(url)
+def Scanner(URL, InFile=open('Dictionary.lst','r').readlines(), OutFile=open('Scan.output', 'a')):
+    parser = urlparse.urlparse(URL)
     if parser.netloc == '':
-        sys.exit("\033[1;31;40m[-]The URL is incorrect!\033[0m")
-    url = (parser.scheme == "http" or parser.scheme == "https") and parser.scheme + "://" + parser.netloc + '/' or "http://" + parser.netloc + '/'
-#    url = parser.path == '/' and url or url + "/"
-    print url
+        sys.exit("\033[1;31m[-]The URL is incorrect!(eg:http://www.baidu.com)\033[0m")
+    URL = (parser.scheme == "http" or parser.scheme == "https") and parser.scheme + "://" + parser.netloc + '/' or "http://" + parser.netloc + '/'
+#    print URL
+    exist = '' #存放返回值200的URL
+    wrong = '' #存放服务器内部错误的URL
+    redirect = '' #重定向的URL
+    print "Result:"
+    for urlpath in InFile:
+        while urlpath.startswith('/'):
+            urlpath = urlpath[1:]
+        try:
+            url = URL + urlpath.strip()
+            status = requests.get(url).status_code
+            if status not in range(400, 418):
+                print  "\033[1;33mFound %s\tStatus: %s\033[0m" % (url, str(status))
+                if status in range(200, 207):
+                    exist += url + '\t' + str(status) +  '\n'
+                if status == 500:
+                    wrong += url + '\t' + str(status) + '\n'
+                if status in range(300, 308):
+                    redirect += url + '\t' +  str(status) + '\n'
+        except (IOError, EOFError, KeyboardInterrupt), e:
+            choice = raw_input('Are you Sure to Stop the Scan?(yes/y,default:no)')
+            if choice == 'yes' or choice == 'y':
+                break 
+    OutFile.write('Target: ' + parser.netloc + "\t\tTime: " + ctime() + '\nExist:\n' + exist + '\nWrong:\n' + wrong + '\nRedirect\n' + redirect + '\n----------------------------------------------------------------\n\n')
+    OutFile.close()
+    print 'GoodBye～'
+
+
+
 
 
 
@@ -49,8 +77,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="DirBuster v{} - T00l for Web Directory Scan".format(DirBuster_version),version=DirBuster_version,usage='dirbuster.py -u [target url] [dirbuster options] ',fromfile_prefix_chars='@', epilog="Time to go~")
     mgroup = parser.add_argument_group("DirBuster", "Options for DirBuster")
     mgroup.add_argument("-u","--url=",metavar="URL",dest="URL",help="The Target URL(eg:http://www.baidu.com/)")
-    mgroup.add_argument("-w","--write",dest="OutFile", type=argparse.FileType('w'),metavar="filename", default=sys.stdout, help="Specify file to log to (stdout by default).")
-    mgroup.add_argument("-f","--file=",dest="InFile", type=argparse.FileType('r'),metavar="filename", default=sys.stdin, help="The Specify file of Directory file.")
+    mgroup.add_argument("-w","--write",dest="OutFile", type=argparse.FileType('w'),metavar="filename", default=sys.stdout, help="Specify file to log to (stdout by default:Scan.output)")
+    mgroup.add_argument("-f","--file=",dest="InFile", type=argparse.FileType('r'),metavar="filename", default=sys.stdin, help="The Specify file of Directory file.(default:Dictionary.lst)")
     args = parser.parse_args()
 
     if len(sys.argv) == 1:
